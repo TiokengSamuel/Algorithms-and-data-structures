0367de2e1317a84cc65428f898ee5817805dadf9
SamuelEdward <samueledward40@gmail.com>
2020-05-07 06:44:24 -0700

    Implementing stacks with list array

---
+++ Implementing stacks with list.py
@@ -0,0 +1,23 @@
+
+#Implementing stacks with python arrays or list
+
+stack = []
+
+stack.append('a')
+stack.append('b')
+stack.append('c')
+
+print("Initial Stack")
+print(stack)
+
+#pop function pops out value in LIFO
+
+print("\n Elements with pop function")
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+#Stack after pop
+print("\n After pop stack")
+print(stack)
+
