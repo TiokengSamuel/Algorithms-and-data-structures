6d696d581d050181a6bc1455e10e4d189aa8bca9
SamuelEdward <samueledward40@gmail.com>
2020-05-09 23:31:31 -0700

    Creating a binary tree

---
+++ Binary tree task 01.py
@@ -0,0 +1,51 @@
+class Node(object):
+    def __init__(self, value=None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def get_value(self):
+        return self.value
+
+    def set_value(self, value):
+        self.value = value
+
+    def get_left_child(self):
+        return self.value
+
+    def set_left_child(self, node):
+        self.left = node
+
+    def get_right_child(self):
+        return self.value
+
+    def set_right_child(self, node):
+        self.right = node
+
+    def has_left_child(self):
+        return self.left != None
+
+    def has_right_child(self):
+        return self.right != None
+
+
+# Defining Tree class
+class Tree(object):
+    def __init__(self, value):
+        self.root = Node(value)
+
+    def get_root(self):
+        return self.root
+
+
+# Driver code
+node0 = Node("apple")
+node1 = Node("banana")
+node2 = Node("orange")
+node0.set_left_child(node1)
+node0.set_right_child(node2)
+print(f"""
+node 0: {node0.value}
+node 0 left child: {node0.left.value}
+node 0 right child: {node0.right.value}
+""")
