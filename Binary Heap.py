6c9244ab0cf89550fac61cd4328ebe0ed8730aee
SamuelEdward <samueledward40@gmail.com>
2020-05-17 16:04:59 -0700

    list 2

--- Binary Heap.py
+++ Binary Heap.py
@@ -1,7 +1,16 @@
 # binary heap
 # heap list 1
-
 class BinHeap:
     def __init__(self):
         self.heapList = [0]
         self.currentSize = 0
+
+    # listing 2
+    def percUp(self, i):
+        while i // 2 > 0:
+            if self.heapList[i] < self.heapList[i // 2]:
+                tmp = self.heapList[i // 2]
+                self.heapList[i // 2] == self.heapList[i]
+
+                self.heapList[i] = tmp
+                i = i // 2
