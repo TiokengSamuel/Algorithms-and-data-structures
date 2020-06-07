03a0560a339201bb6b63b07adea14cdd7c4bbdab
SamuelEdward <samueledward40@gmail.com>
2020-06-07 00:01:56 -0700

    making program smart with decorators

--- decorators.py
+++ decorators.py
@@ -2,4 +2,13 @@ def div(a, b):
     print(a / b)
 
 
+def smart_div(func):
+    def inner(a, b):
+        if a > b:
+            a, b = b, a
+            return func(a, b)
+
+    return inner
+
+
 print(div(2, 4))
